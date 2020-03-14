2a95fb78a755eae96d6be4c27d7d0d75f328c1b3
alkaitagi <alkaitagi@outlook.com>
2020-03-14 21:44:02 +0300

    Fix caching url args

--- cache_helper.py
+++ cache_helper.py
@@ -1,11 +1,16 @@
 import glob
+import json
 
 cache = ['index.html', 'icon.png', 'manifest.json', 'pwabuilder-sw.js']
-roots = ['scripts', 'styles', 'languages']
+
+for lang in json.load(open('languages/catalogue.json', "r")):
+   cache.append(f"index.html?&language={lang}")
+
+roots=['scripts', 'styles', 'languages']
 for root in roots:
     cache.extend(glob.glob(root + '/**/*.*', recursive=True))
 
-f = open("cachehelper.txt", "w", encoding="utf-8")
+f=open("cachehelper.txt", "w", encoding="utf-8")
 for c in cache:
     f.write("\"" + c.replace('\\', '/') + "\",\n")
 f.close()
