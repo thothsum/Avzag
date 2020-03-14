4512bc4344d64e3389b73fa138bce29e41b8f350
alkaitagi <alkaitagi@outlook.com>
2020-03-14 21:05:32 +0300

    Fix caching

--- cache_helper.py
+++ cache_helper.py
@@ -1,11 +1,11 @@
 import glob
 
-cache = ['index.html', 'icon.png, manifest.json']
+cache = ['index.html', 'icon.png', 'manifest.json']
 roots = ['scripts', 'styles', 'languages']
 for root in roots:
     cache.extend(glob.glob(root + '/**/*.*', recursive=True))
 
 f = open("cachehelper.txt", "w", encoding="utf-8")
 for c in cache:
-    f.write("'" + c.replace('\\', '/') + "',\n")
+    f.write("\"" + c.replace('\\', '/') + "\",\n")
 f.close()
