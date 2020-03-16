957d813a5b925900763c1086a8c54587f3f699ee
alkaitagi <alkaitagi@outlook.com>
2020-03-16 04:25:14 +0300

    Add current tab header hightlight

--- scripts/header.mjs
+++ scripts/header.mjs
@@ -1,11 +1,17 @@
 let currentTab;
+let currentTabNav;
 
 
-function switchTab(tab) {
-    if (currentTab != null)
+function switchTab(nav, tab) {
+    if (currentTab != null) {
         currentTab.style.display = "none";
+        currentTabNav.className = "tab-nav";
+    }
     currentTab = tab;
+    currentTabNav = nav;
+
     currentTab.style.display = "block";
+    currentTabNav.className = "tab-nav current";
 }
 
 
@@ -13,6 +19,6 @@ let tabNavs = document.querySelectorAll("#header .tab-nav");
 for (const tabNav of tabNavs) {
     let tabTarget = document.querySelector(`#${tabNav.innerText.toLowerCase()}`);
     tabTarget.style.display = "none";
-    tabNav.onclick = () => switchTab(tabTarget);
+    tabNav.onclick = () => switchTab(tabNav, tabTarget);
 }
 tabNavs[0].click();
