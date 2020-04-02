72f419bac79bdec2da49d3c1c5815ff31f461ef7
alkaitagi <alkaitagi@outlook.com>
2020-04-02 04:56:58 +0300

    Bug fix

--- src/components/PhoneticTable.vue
+++ src/components/PhoneticTable.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="table">
-    <h3>{{category}}s</h3>
+    <h3>{{title}}s</h3>
     <div class="query">
-      <QueryItem :key="i" :tag="tag" @query="addQuery(tag, $event)" v-for="(tag, i) in allTags()" />
+      <QueryItem :key="i" :tag="tag" @query="addQuery(tag, $event)" v-for="(tag, i) in tags" />
     </div>
     <PhonemeItem
-      :phoneme="phonemes[i]"
-      :faded="!queried.includes(i)"
-      :key="i"
-      v-for="i in doQuery(phonemes)"
-      @click.native="$emit('phoneme', i)"
+      :phoneme="phn"
+      :faded="!results.includes(phn)"
+      :key="phn.i"
+      v-for="phn in phonemes"
+      @click.native="$emit('phoneme', phn.i)"
     />
   </div>
 </template>
@@ -28,33 +28,37 @@ export default {
   data() {
     return {
       query: [],
-      queried: []
+      results: []
     };
   },
-  created() {
-    this.queried = this.phonemes.map(p => p.i);
-  },
-  methods: {
-    doQuery(query) {
-      let results = this.phonemes.map(p => p.i);
-      Object.keys(query).forEach(
-        tag =>
-          (results = results.filter(
-            i => query[tag] === this.phonemes[i].tags.includes(tag)
-          ))
-      );
-      return results;
-    },
-    allTags() {
+  computed: {
+    tags: function() {
       let set = new Set();
       this.phonemes.forEach(p => p.tags?.forEach(t => set.add(t)));
       set.delete(this.category);
       return set;
     },
+    title: function() {
+      return this.category[0].toUpperCase() + this.category.slice(1);
+    }
+  },
+  created() {
+    this.getResults();
+  },
+  methods: {
     addQuery(tag, mode) {
       if (mode === 0) delete this.query[tag];
       else this.query[tag] = mode === 1 ? true : false;
-      this.queried = this.sortBy(this.query);
+      this.getResults();
+    },
+    getResults() {
+      let results = this.phonemes;
+      Object.keys(this.query).forEach(
+        t =>
+          (results = results.filter(r => this.query[t] === r.tags.includes(t)))
+      );
+      this.results = results;
+      console.log(results);
     }
   }
 };
@@ -73,5 +77,18 @@ export default {
 
 h3 {
   width: 100%;
+}
+
+.query {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  margin: 5px 0;
+}
+
+.query > p {
+  margin: 0 5px;
+  font-style: italic;
+  font-size: 12px;
 }
 </style>
