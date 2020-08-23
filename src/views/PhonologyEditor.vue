375c985b9431c837b805a23688be34f78a46fe0f
alkaitagi <alkaitagi@outlook.com>
2020-08-23 21:10:22 +0300

    Editor phoneme assign & delete

--- src/views/PhonologyEditor.vue
+++ src/views/PhonologyEditor.vue
@@ -5,26 +5,32 @@
         <h3>{{t}}</h3>
         <div class="table panel-horizontal-dense wrap">
           <PhoneticItem
-            @click.native="selected=p"
-            :selected="selected==p"
+            @click.native="selectPhoneme(t,p)"
+            :selectedCopy="selectedCopy && selectedCopy.phoneme==p"
             :ipa="p"
             :str="Object.keys(u.samples)[0]"
             :key="p"
             v-for="(u, p) of file[t]"
           />
-          <!-- <h2>{{p}}</h2>
-        <h3 v-if="u.notes">Notes</h3>
-        <p :key="i" v-for="(n,i) in u.notes">{{n}}</p>
-        <h3 v-if="u.samples">Samples</h3>
-        <div :key="g" v-for="(s, g) of u.samples" class="panel-horizontal">
-          {{g}}
-          <div :key="i" v-for="(w,i) in s">
-            <input type="text" v-model="w.word" />
-            <input type="text" v-model="w.ipa" />
-            <input type="checkbox" v-model="w.muted" />
-          </div>
-          </div>-->
           <button class="add" @click="addPhoneme(t)">+</button>
+        </div>
+      </div>
+    </div>
+    <div class="card panel" v-if="selectedCopy">
+      <button @click="deletePhoneme">delete</button>
+      <button @click="applyPhoneme">apply</button>
+      <input type="text" v-model="selectedCopy.phoneme" />
+      <h3 v-if="selectedCopy.notes">Notes</h3>
+      <textarea v-model="selectedCopy.notes[i]" :key="i" v-for="(n,i) in selectedCopy.notes" />
+      <h3 v-if="selectedCopy.samples">Samples</h3>
+      <div :key="g" v-for="(s, g) of selectedCopy.samples" class="panel">
+        {{g}}
+        <div :key="i" v-for="(w,i) in s" class="panel-dense">
+          <input type="text" v-model="w.word" />
+          <input type="text" v-model="w.ipa" />
+          <div class="panel-horizontal">
+            <input type="checkbox" v-model="w.muted" />muted
+          </div>
         </div>
       </div>
     </div>
@@ -43,12 +49,30 @@ export default {
     return {
       types: ["vowels", "consonants"],
       file: null,
-      selected: null,
+      selectedType: null,
+      selectedPhoneme: null,
+      selectedCopy: null,
     };
   },
   computed: {
     text() {
       return JSON.stringify(this.file);
+    },
+  },
+  watch: {
+    selectedCopy(val) {
+      let data = null;
+      for (const t of this.types) {
+        data = this.file[t][this.selectedCopy];
+        if (data) {
+          data.phoneme = val;
+          break;
+        }
+      }
+      this.selectedCopyData = data;
+    },
+    file() {
+      this.$forceUpdate();
     },
   },
   async mounted() {
@@ -61,10 +85,30 @@ export default {
     this.file = await fetch(`${r}${l}/phonemes.json`).then((r) => r.json());
   },
   methods: {
+    selectPhoneme(t, p) {
+      this.selectedType = t;
+      this.selectedPhoneme = p;
+
+      this.selectedCopy = JSON.parse(JSON.stringify(this.file[t][p]));
+      this.selectedCopy.phoneme = p;
+      this.selectedCopy.type = t;
+    },
     addPhoneme(type) {
       if ("new" in this.file[type]) return;
       this.file[type]["new"] = { samples: { new: [] } };
-      this.$forceUpdate();
+    },
+    deletePhoneme() {
+      delete this.file[this.selectedCopy.type][this.selectedCopy.phoneme];
+      this.selectedCopy = null;
+    },
+    applyPhoneme() {
+      const t = this.selectedCopy.type;
+      const p = this.selectedCopy.phoneme;
+      delete this.selectedCopy.type;
+      delete this.selectedCopy.phoneme;
+      delete this.file[this.selectedType][this.selectedPhoneme];
+      this.file[t][p] = JSON.parse(JSON.stringify(this.selectedCopy));
+      this.selectPhoneme(t, p);
     },
   },
 };
