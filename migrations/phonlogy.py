0f0b1d27a1b24191e6c52137ed108a64418785d8
alkaitagi <alkaitagi@outlook.com>
2020-09-26 11:24:36 +0300

    Migration

--- migrations/phonlogy.py
+++ migrations/phonlogy.py
@@ -7,13 +7,14 @@ source = json.loads(open("public/languages/"+fileName+"/phonology.json",
                          "r", encoding="utf8").read())
 result = []
 for old in source:
-    samples = []
-    for u in old["uses"][lectName]:
-        for s in u["samples"]:
-            samples.append(
-                {"grapheme": u["grapheme"], "word": s[0], "ipa": s[1]})
+    if lectName in old["uses"]:
+        samples = []
+        for u in old["uses"][lectName]:
+            for s in u["samples"]:
+                samples.append(
+                    {"grapheme": u["grapheme"], "word": s[0], "ipa": s[1]})
+        result.append({"phoneme": old["ipa"], "samples": samples})
 
-    result.append({"phoneme": old["ipa", "samples": samples]})
 
 open("public/lects/"+lectName+"/phonology.json", "w",
      encoding="utf8").write(json.dumps(result, ensure_ascii=False, indent=4))
