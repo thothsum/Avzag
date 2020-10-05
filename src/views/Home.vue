c45b9023507f37cc1cf97bb9cf5930d960cc810f
alkaitagi <alkaitagi@outlook.com>
2020-10-06 01:23:54 +0300

    Add leaflet map

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,43 @@
 <template>
-  <div class="panel-sparse" id="root">
-    <div id="header" class="panel-horizontal wrap">
-      <div class="panel-horizontal-sparse" id="title">
-        <h1>Ævzag</h1>
-        <button v-if="!empty" @click="load" class="highlight">
-          <h1 class="icon">arrow_forward</h1>
-        </button>
-      </div>
-      <div class="panel-horizontal">
-        <a href="https://github.com/alkaitagi/Avzag#contacts">Contacts</a>
-        <a href="https://github.com/alkaitagi/Avzag#credits">Credits</a>
-        <span class="text-dot"></span>
-        <a href="https://github.com/alkaitagi/Avzag">GitHub</a>
-      </div>
+  <div id="root">
+    <div id="map"></div>
+    <div id="ui">
+      <h2>UI layer</h2>
+      <button>Hello there</button>
+      <!-- <div id="header" class="panel-horizontal wrap">
+        <div class="panel-horizontal-sparse" id="title">
+          <h1>Ævzag</h1>
+          <button v-if="!empty" @click="load" class="highlight">
+            <h1 class="icon">arrow_forward</h1>
+          </button>
+        </div>
+        <div class="panel-horizontal">
+          <a href="https://github.com/alkaitagi/Avzag#contacts">Contacts</a>
+          <a href="https://github.com/alkaitagi/Avzag#credits">Credits</a>
+          <span class="text-dot"></span>
+          <a href="https://github.com/alkaitagi/Avzag">GitHub</a>
+        </div>
+      </div> -->
     </div>
-    <LectFamily
+    <!-- <LectFamily
       @select="select"
       :key="i"
       v-for="(f, i) in catalogue"
       :family="f"
-    />
+    /> -->
   </div>
 </template>
 
 <script>
-import LectFamily from "@/components/LectFamily";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// import LectFamily from "@/components/LectFamily";
 
 export default {
   name: "Home",
   components: {
-    LectFamily,
+    // LectFamily,
   },
   data() {
     return {
@@ -41,6 +49,22 @@ export default {
     catalogue() {
       return this.$store.state.catalogue;
     },
+  },
+  mounted() {
+    var map = L.map("map").setView([51.505, -0.09], 13);
+
+    L.tileLayer(
+      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+      {
+        // attribution:
+        //   'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 12,
+        id: "mapbox/light-v10",
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: process.env.VUE_APP_MAP_TOKEN,
+      }
+    ).addTo(map);
   },
   methods: {
     select(lect, incl) {
@@ -57,19 +81,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-#header {
-  z-index: 2;
-  justify-content: space-between;
-  background-color: var(--color-foreground);
-  border-radius: 0;
-  box-shadow: map-get($shadows, "elevated");
-  width: calc(100% + map-get($margins, "normal"));
-  margin: -1 * map-get($margins, "normal");
-  padding: map-get($margins, "normal");
+#root {
+  position: relative;
+  width: 100%;
+  height: 100%;
 }
-#title {
-  h1 {
-    flex: 0;
+
+#map {
+  height: 100vh;
+  width: 100vw;
+  left: -1 * map-get($margins, "normal");
+  top: -1 * map-get($margins, "normal");
+  z-index: 1;
+  position: absolute;
+}
+
+#ui {
+  position: absolute;
+  z-index: 10;
+  width: 512px;
+  background-color: transparent;
+  pointer-events: none;
+  * {
+    pointer-events: auto;
   }
 }
 
