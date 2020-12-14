892ba7dcf4f447f356c6286531c35b032ace67fe
alkaitagi <alkaitagi@outlook.com>
2020-12-15 00:19:17 +0300

    Fix ESLint errors

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,12 @@
-import { createApp } from 'vue'
-import router from './router'
-import store from './store'
-import App from './App.vue'
+import { createApp } from "vue";
+import router from "./router";
+import store from "./store";
+import App from "./App.vue";
 
-import './registerServiceWorker'
-import 'leaflet/dist/leaflet.css'
+import "./registerServiceWorker";
+import "leaflet/dist/leaflet.css";
 
-// new Vue({
-//   router,
-//   store,
-//   render: h => h(App)
-// }).$mount('#app')
-
-const app = createApp(App)
-app.use(router)
-app.use(store)
-router.isReady().then(() => app.mount('#app'))
+const app = createApp(App);
+app.use(router);
+app.use(store);
+router.isReady().then(() => app.mount("#app"));
