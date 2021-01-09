7c49175e9e2310877761dc1186177969a2c37a6f
alkaitagi <alkaitagi@outlook.com>
2021-01-09 04:32:10 +0300

    EditorNav control buttons

--- src/components/EditorNavHeader.vue
+++ src/components/EditorNavHeader.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="root" v-bind="attrs" class="small">
-    <div class="section row-1 scroll">
-      <router-link v-slot="{ navigate }" custom :to="{ name: 'Home' }">
-        <control icon="arrow_back" @click="navigate" />
-      </router-link>
-      <Select v-model="menu" :items="menus" label-key="text" />
+    <div class="section scroll row-1">
+      <div class="row-1">
+        <router-link v-slot="{ navigate }" custom :to="{ name: 'Home' }">
+          <control icon="arrow_back" @click="navigate" />
+        </router-link>
+        <Select v-model="menu" :items="menus" label-key="text" />
+      </div>
+      <div class="row-1 controls">
+        <control icon="language" />
+        <control icon="code" />
+        <control icon="integration_instructions" />
+        <ButtonAlert />
+      </div>
     </div>
   </div>
   <router-view />
@@ -12,6 +20,7 @@
 
 <script setup lang="ts">
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import ButtonAlert from "./ButtonAlert";
 import Select from "./Select";
 import { ref, useContext, watch } from "vue";
 import { useRouter } from "vue-router";
@@ -56,5 +65,9 @@ watch(menu, ({ name }) => router.push({ name }));
 }
 :not(.icon) {
   font-weight: bold;
+}
+.controls {
+  flex: 1;
+  justify-content: flex-end;
 }
 </style>
