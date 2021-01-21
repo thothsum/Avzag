c582bc43630e8b4efef424c7d09bf16dd04b3345
alkaitagi <alkaitagi@outlook.com>
2021-01-22 00:12:57 +0300

    Fixes

--- src/Phrasebook/State/ConditionsEditor.vue
+++ src/Phrasebook/State/ConditionsEditor.vue
@@ -2,13 +2,13 @@
   <EditorCard icon="tune" header="conditions">
     <template #caption>TODO brief explanation</template>
     <div class="row wrap">
-      <template v-for="{ entity, tags } in context">
+      <template v-for="(cs, i) in colors">
         <btn
-          v-for="tag in tags"
-          :key="entity + '--' + tag"
-          :text="tag"
-          :class="getColor(entity, tag)"
-          @click="toggle(entity, tag)"
+          v-for="(c, j) in cs"
+          :key="i + '--' + j"
+          :text="context[i].tags[j]"
+          :class="c"
+          @click="toggle(i, j)"
         />
       </template>
     </div>
@@ -17,7 +17,14 @@
 
 <script lang="ts">
 import EditorCard from "@/components/EditorCard.vue";
-import { computed, defineComponent, PropType, inject, ComputedRef } from "vue";
+import {
+  computed,
+  defineComponent,
+  PropType,
+  inject,
+  ComputedRef,
+  ref,
+} from "vue";
 import { ContextSource, Conditions } from "../types";
 
 export default defineComponent({
@@ -31,35 +38,47 @@ export default defineComponent({
       "contextSource",
       {} as ComputedRef<undefined | ContextSource[]>
     );
-    const entityColors = computed(() =>
-      context.value?.map((_, i) => "colored-dot-" + i)
-    );
 
     const conditions = computed({
       get: () => props.modelValue,
       set: (c) => emit("update:modelValue", c),
     });
 
-    function toggle(entity: string, tag: string) {
+    function toggle(ei: number, ti: number) {
+      if (!context.value) return;
+      const entity = context.value[ei].entity;
+      const tag = context.value[ei].tags[ti];
+
       if (!conditions.value[entity]) conditions.value[entity] = {};
       const condition = conditions.value[entity];
 
-      if (condition[tag]) {
+      if (condition[tag] === undefined) condition[tag] = 1;
+      else {
         condition[tag] -= 1;
         if (condition[tag] < -1) {
           delete condition[tag];
           if (!Object.keys(condition).length) delete conditions.value[entity];
         }
-      } else condition[tag] = 1;
-      conditions.value[entity] = condition;
+      }
+      refreshColors();
     }
 
-    function getColor(entity: string, tag: string) {
-      const flag = conditions.value[entity]?.[tag];
-      if (flag === undefined) return "";
-      return "colored-border-" + (flag + 1);
+    const colors = ref([] as string[][]);
+    function refreshColors() {
+      if (!context.value) return;
+      colors.value = [] as string[][];
+      context.value.forEach(({ entity, tags }, i) => {
+        colors.value.push([]);
+        tags.forEach((tag) => {
+          let flag = conditions.value[entity]?.[tag] as number;
+          flag = flag === undefined ? -1 : flag + 1;
+          colors.value[i].push(`colored-dot-${i} colored-border-${flag}`);
+        });
+      });
     }
-    return { context, conditions, entityColors, getColor, toggle };
+    refreshColors();
+
+    return { context, conditions, colors, toggle };
   },
 });
 </script>
