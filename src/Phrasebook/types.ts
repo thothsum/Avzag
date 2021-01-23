64c3d788bc5fab29e740b6a40e993881adec6bfe
alkaitagi <alkaitagi@outlook.com>
2021-01-23 04:03:30 +0300

    Update condition type

--- src/Phrasebook/types.ts
+++ src/Phrasebook/types.ts
@@ -1,4 +1,4 @@
-export type ConditionType = { provide: boolean; require: boolean };
+export type ConditionType = { provide?: boolean; require?: boolean };
 export type Conditions = Record<string, Record<string, ConditionType>>;
 export type Text = {
   plain: string;
