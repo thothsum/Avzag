3cdf97c09b415b27b8032fb7c9424eb955db3fa7
alkaitagi <alkaitagi@outlook.com>
2021-01-29 14:49:23 +0300

    Add queue to playback API

--- src/audio-player.ts
+++ src/audio-player.ts
@@ -1,10 +1,19 @@
 const audio = new Audio();
 
+let queue: string[];
+function next() {
+  const src = queue.pop();
+  if (src) audio.src = src;
+}
+
 audio.oncanplaythrough = () => {
   console.log("ready");
   audio.play();
 };
 
-export function play(src: string) {
-  audio.src = src;
+audio.onended = next;
+
+export function play(...srcs: string[]) {
+  queue = srcs.slice();
+  next();
 }
