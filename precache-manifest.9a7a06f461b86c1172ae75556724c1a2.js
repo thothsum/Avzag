7f71f0195728e8e205ba0b32d6a9af85fe0f8164
alkaitagi <alkaitagi@outlook.com>
2021-01-29 21:17:11 +0000

    Deploying to dist from  @ aaaa1b872bc53d24d6ed837012abd1b333bd54eb 🚀

--- precache-manifest.9a7a06f461b86c1172ae75556724c1a2.js
+++ precache-manifest.9a7a06f461b86c1172ae75556724c1a2.js
@@ -4,12 +4,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/404.html"
   },
   {
-    "revision": "fc78f2632ad044e10983",
+    "revision": "574a55ed67fb6af52e47",
     "url": "/css/app.254be65d.css"
   },
   {
-    "revision": "bb764ddf94d9d2dd74ff",
-    "url": "/css/chunk-21e0f259.ba6426a0.css"
+    "revision": "f41831bf990d51b056fe",
+    "url": "/css/chunk-0941d0e2.feffbec0.css"
   },
   {
     "revision": "69c886eb99edbf663f9e",
@@ -28,6 +28,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/css/chunk-348a9097.5908898e.css"
   },
   {
+    "revision": "a363a74f30f5bc11a0a1",
+    "url": "/css/chunk-41432b02.8348473b.css"
+  },
+  {
     "revision": "b085fb32dd22d982c734",
     "url": "/css/chunk-48c2c050.e95cc0a4.css"
   },
@@ -36,8 +40,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/css/chunk-4e1d0016.a31de994.css"
   },
   {
-    "revision": "ec2802e38923f0002b88",
-    "url": "/css/chunk-60fb171c.a8ec4bf9.css"
+    "revision": "e93797db96cac3a13db5",
+    "url": "/css/chunk-5d547f8e.7a9764af.css"
   },
   {
     "revision": "43652ceb0dff9dedd808",
@@ -48,28 +52,28 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/css/chunk-938df288.e80d4245.css"
   },
   {
-    "revision": "91c06d05edaae1d68142",
-    "url": "/css/chunk-af5e4a56.2bb19b99.css"
-  },
-  {
     "revision": "c33799a303656ee2db545ddbe182bbef",
     "url": "/icon.png"
   },
   {
-    "revision": "c59cd39735221a6d5612c43a0a6dc493",
+    "revision": "a06728caee3cb486bbe71dca7c4e7da9",
     "url": "/index.html"
   },
   {
-    "revision": "fc78f2632ad044e10983",
-    "url": "/js/app.c5ec0b34.js"
+    "revision": "574a55ed67fb6af52e47",
+    "url": "/js/app.667955f6.js"
   },
   {
-    "revision": "bb764ddf94d9d2dd74ff",
-    "url": "/js/chunk-21e0f259.f6f3e62d.js"
+    "revision": "f41831bf990d51b056fe",
+    "url": "/js/chunk-0941d0e2.58c4d6a8.js"
   },
   {
     "revision": "69c886eb99edbf663f9e",
     "url": "/js/chunk-2f16ffbf.8a16e99a.js"
+  },
+  {
+    "revision": "cc8569c8f6705a154573",
+    "url": "/js/chunk-30508e78.0500ca87.js"
   },
   {
     "revision": "c42207357ccd8e9e5e05",
@@ -84,20 +88,24 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/js/chunk-348a9097.97e65efa.js"
   },
   {
+    "revision": "a363a74f30f5bc11a0a1",
+    "url": "/js/chunk-41432b02.c73df7cc.js"
+  },
+  {
     "revision": "b085fb32dd22d982c734",
     "url": "/js/chunk-48c2c050.6844ec22.js"
+  },
+  {
+    "revision": "e9804d2d817e4bf73c07",
+    "url": "/js/chunk-4a994a59.ec294396.js"
   },
   {
     "revision": "4be4148bd4ff5a24cac4",
     "url": "/js/chunk-4e1d0016.a77c747f.js"
   },
   {
-    "revision": "91b7ef886d5b14d843f7",
-    "url": "/js/chunk-572359ac.54bb751d.js"
-  },
-  {
-    "revision": "ec2802e38923f0002b88",
-    "url": "/js/chunk-60fb171c.79be6fe6.js"
+    "revision": "e93797db96cac3a13db5",
+    "url": "/js/chunk-5d547f8e.31032f4a.js"
   },
   {
     "revision": "43652ceb0dff9dedd808",
@@ -108,12 +116,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/js/chunk-938df288.f494ba5a.js"
   },
   {
-    "revision": "91c06d05edaae1d68142",
-    "url": "/js/chunk-af5e4a56.1e7412d1.js"
-  },
-  {
-    "revision": "7796723c8a12b3c8fcc5",
-    "url": "/js/chunk-vendors.32556aeb.js"
+    "revision": "76eccf47daeac7637f66",
+    "url": "/js/chunk-vendors.3fdab4be.js"
   },
   {
     "revision": "6d4a5d6d42c2de3525460b663a37abf4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.05eff4b512df128afc174c3a3e3bbc47.js"
+  "/precache-manifest.9a7a06f461b86c1172ae75556724c1a2.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "avzag"});
