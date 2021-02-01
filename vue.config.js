c9630a5bf700095ed5e30d49c7def02f6074b6a3
alkaitagi <alkaitagi@outlook.com>
2021-02-01 16:51:36 +0300

    Restore icons in config

--- vue.config.js
+++ vue.config.js
@@ -23,5 +23,12 @@ module.exports = {
         },
       ],
     },
+    iconPaths: {
+      favicon32: "icon.png",
+      favicon16: "icon.png",
+      appleTouchIcon: "icon.png",
+      maskIcon: "icon.png",
+      msTileImage: "icon.png",
+    },
   },
 };
