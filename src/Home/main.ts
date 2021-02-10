69354081bea06faa69704643ded992ad8e5f43f4
alkaitagi <alkaitagi@outlook.com>
2021-02-10 21:45:46 +0300

    Optimize search

--- src/Home/main.ts
+++ src/Home/main.ts
@@ -1,4 +1,4 @@
-import { ref, shallowRef, reactive, computed, watch } from "vue";
+import { ref, shallowRef, reactive, computed } from "vue";
 import { SearchState, Lect } from "./types";
 import { loadJSON } from "@/store";
 import { Query, EvaluateQuery } from "@/components/Query/types";
@@ -6,23 +6,19 @@ import { Query, EvaluateQuery } from "@/components/Query/types";
 export const catalogue = shallowRef<Lect[]>([]);
 export const search = reactive({
   selected: new Set<string>(),
-  visible: new Set<string>(),
-} as SearchState);
+  visible: computed(() =>
+    tags.value.reduce((set, t, i) => {
+      if (EvaluateQuery(t, query.value, true)) set.add(catalogue.value[i].name);
+      return set;
+    }, new Set<string>())
+  ),
+}) as SearchState;
 export const query = ref<Query>({});
 
 const tags = computed(() =>
   catalogue.value.map(({ name, tags, family }) =>
     [name, tags ?? "", family].flat().join(" ").toLowerCase()
   )
-);
-watch(tags, (tags) =>
-  tags.forEach((t, i) => {
-    const name = catalogue.value[i].name;
-    if (!name) return;
-
-    if (EvaluateQuery(t, query.value, true)) search.visible.add(name);
-    else search.visible.delete(name);
-  })
 );
 
 export async function reset() {
@@ -33,8 +29,7 @@ export async function reset() {
       .then((ks) => ks.find((k) => k.includes("avzag-precache")))
       .then((k) => caches.delete(k ?? ""));
 
+  query.value = {};
   catalogue.value = await loadJSON("catalogue", []);
   search.selected.clear();
-  search.visible.clear();
-  query.value = {};
 }
