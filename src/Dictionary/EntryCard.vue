78d2bbaa0470c6afe08b67d9716441e53989689d
alkaitagi <alkaitagi@outlook.com>
2021-03-05 23:49:51 +0300

    Improve layout

--- src/Dictionary/EntryCard.vue
+++ src/Dictionary/EntryCard.vue
@@ -1,50 +1,52 @@
 <template>
   <div class="col small">
-    <toggle v-model="expanded.card" class="card">{{
-      entry.forms[0].text.plain
-    }}</toggle>
-    <template v-if="expanded.card">
-      <h2 class="header">
-        <toggle v-model="expanded.forms" text="Forms" class="flex" />
-      </h2>
-      <div class="col-0 card">
-        <temaplate v-if="expanded.forms">
-          <div v-for="(f, i) in entry.forms" :key="i" class="col-0">
-            <p class="row">
-              <span>{{ f.text.plain }}</span>
-              <span class="text-ipa">{{ f.text.ipa }}</span>
-              <span>{{ f.text.glossed }}</span>
-            </p>
-            <p class="text-faded text-caption">{{ f.grammar }}</p>
-          </div>
-        </temaplate>
-      </div>
-      <h2 class="header">
-        <toggle v-model="expanded.samples" text="Samples" class="flex" />
-      </h2>
-      <div v-if="entry.samples" class="col-0 card">
-        <temaplate v-if="expanded.samples">
-          <div v-for="(s, i) in entry.samples" :key="i" class="col-0">
-            <p>{{ s.text.plain }}</p>
-            <p class="text-faded text-caption">{{ s.translation }}</p>
-          </div>
-        </temaplate>
-      </div>
-    </template>
+    <div class="row">
+      <p class="flex">{{ entry.forms[0].text.plain }}</p>
+      <btn
+        icon="format_list_bulleted"
+        :text="expand === 1 ? 'forms' : ''"
+        :is-on="expand === 1"
+        @click="expand = expand === 1 ? 0 : 1"
+      />
+      <btn
+        icon="speaker_notes"
+        :text="expand === 2 ? 'samples' : ''"
+        :is-on="expand === 2"
+        @click="expand = expand === 2 ? 0 : 2"
+      />
+    </div>
+    <div v-if="expand" class="col-0 card">
+      <template v-if="expand === 1">
+        <div v-for="(f, i) in entry.forms" :key="i" class="col-0">
+          <p class="row">
+            <span>{{ f.text.plain }}</span>
+            <span class="text-ipa">{{ f.text.ipa }}</span>
+            <span>{{ f.text.glossed }}</span>
+          </p>
+          <p class="text-faded text-caption">{{ f.grammar }}</p>
+        </div>
+      </template>
+      <template v-else-if="expand === 2">
+        <div v-for="(s, i) in entry.samples" :key="i" class="col-0">
+          <p>{{ s.text.plain }}</p>
+          <p class="text-faded text-caption">{{ s.translation }}</p>
+        </div>
+      </template>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, reactive } from "vue";
+import { defineComponent, PropType, ref } from "vue";
 import { Entry } from "./types";
 
 export default defineComponent({
   name: "EntryCard",
   props: { entry: { type: Object as PropType<Entry>, default: undefined } },
   setup() {
-    const expanded = reactive({ card: false, forms: false, samples: false });
+    const expand = ref(0);
 
-    return { expanded };
+    return { expand };
   },
 });
 </script>
