3aabdbc1153acc2a22bd12a1fb9483361ad91d33
alkaitagi <alkaitagi@outlook.com>
2021-03-07 11:43:39 +0300

    Fix input fields

--- src/Dictionary/index.vue
+++ src/Dictionary/index.vue
@@ -1,21 +1,31 @@
 <template>
   <div class="section col small">
-    <div class="row-2 lects fill">
+    <div class="row-1 lects fill">
       <div class="col card lect">
-        Search by meaning
-        <input v-if="!lect" v-model="query" class="flex" type="text" />
-        <div v-else><btn icon="search" @click="lect = ''" /></div>
+        English
+        <input v-if="!lect" v-model="query" type="text" />
+        <btn
+          v-else
+          icon="search"
+          :text="`Search by meaning...`"
+          @click="lect = ''"
+        />
       </div>
       <div v-for="(_, l) of dictionaries" :key="l" class="col card lect flag">
         <Flag :lect="l" class="blur" />
         <h2 class="flex">{{ l }}</h2>
         <input v-if="lect === l" v-model="query" type="text" />
-        <div v-else><btn icon="search" @click="lect = l" /></div>
+        <btn
+          v-else
+          icon="search"
+          :text="`Search by ${l} form...`"
+          @click="lect = l"
+        />
       </div>
     </div>
     <template v-for="(ind, m) of searchResult" :key="m">
       <hr />
-      <div class="row-2 lects">
+      <div class="row-1 lects">
         <i class="lect text-faded">{{ m }}</i>
         <div v-for="(es, l) of ind" :key="l" class="col lect">
           <EntryCard v-for="(e, i) in es" :key="i" :entry="e" />
@@ -26,7 +36,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from "vue";
+import { computed, defineComponent, reactive, ref } from "vue";
 import { search, dictionaries } from "./main";
 import EntryCard from "./EntryCard.vue";
 import Flag from "@/components/Flag.vue";
@@ -34,11 +44,14 @@ import Flag from "@/components/Flag.vue";
 export default defineComponent({
   components: { EntryCard, Flag },
   setup() {
-    const query = ref("");
+    const queries = reactive({} as Record<string, string>);
     const lect = ref("");
+    const query = computed({
+      get: () => queries[lect.value],
+      set: (q) => (queries[lect.value] = q),
+    });
 
     const searchResult = computed(() => search(lect.value, query.value));
-
     return { dictionaries, query, lect, searchResult };
   },
 });
