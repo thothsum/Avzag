2488345af67888b3345d9653ee5c679e8cc4e96d
alkaitagi <alkaitagi@outlook.com>
2021-03-08 08:17:20 +0300

    Add migrations

---
+++ src/Dictionary/migrations/meaning2translation.py
@@ -0,0 +1,14 @@
+import os
+import json
+
+path = os.path.dirname(os.path.abspath(__file__))+'\\'
+data = []
+with open(path+"mn.json", encoding='utf-8') as f:
+    data = json.loads(f.read())
+
+for entry in data:
+    entry["translation"] = entry["meaning"]["primary"]
+    del entry["meaning"]
+
+with open(path+'tr.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
