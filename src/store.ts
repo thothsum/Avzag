eba71d33dfdb65f660f4b6886dbbbf74397366d0
alkaitagi <alkaitagi@outlook.com>
2021-05-01 17:44:34 +0300

    Add donwloading delay

--- src/store.ts
+++ src/store.ts
@@ -23,7 +23,7 @@ export async function checkOutdated(prompt = false) {
   const outdated = [];
   for (const [path, { added }] of entries) {
     const updated = await lastCommitTime(path);
-    if (updated > added + 10000) outdated.push(path);
+    if (updated > added + 600000) outdated.push(path);
   }
 
   if (outdated.length) {
