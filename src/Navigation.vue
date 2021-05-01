a01c0bbf0f000b545ed75098ec9c69318857b34b
alkaitagi <alkaitagi@outlook.com>
2021-05-01 18:18:15 +0300

    Remove test

--- src/Navigation.vue
+++ src/Navigation.vue
@@ -4,9 +4,6 @@
       <router-link to="/home">
         <btn icon="arrow_back" />
       </router-link>
-      <router-link to="/home">
-        <btn icon="arrow_back" />
-      </router-link>
       <router-link
         v-for="{ icon, name } in userRoutes"
         :key="name"
