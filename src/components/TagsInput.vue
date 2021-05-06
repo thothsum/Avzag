637bc3e4ea70a8f7f9497616df185baaaa7517ae
alkaitagi <alkaitagi@outlook.com>
2021-05-06 18:25:02 +0300

    Add tags input component

---
+++ src/components/TagsInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <input v-model="text" type="text" />
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  name: "ContextEditor",
+  props: {
+    modelValue: {
+      type: Array as PropType<string[]>,
+      default: () => [],
+    },
+  },
+  emits: ["update:modelValue"],
+  setup(props, { emit }) {
+    const text = computed({
+      get: () => props.modelValue.join(" "),
+      set: (t) => emit("update:modelValue", t.split(" ")),
+    });
+    return { text };
+  },
+});
+</script>
