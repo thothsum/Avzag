03f1169cc4cfaa2692545a45633ce91fe860035b
alkaitagi <alkaitagi@outlook.com>
2021-05-07 00:22:30 +0300

    Rename file

--- src/Dictionary/main.ts
+++ src/Dictionary/main.ts
@@ -8,5 +8,5 @@ export const dictionaries = shallowRef({} as Record<string, Entry[]>);
 watch(lects, async () => {
   dictionaries.value = {};
   dictionaries.value = await loadLectsJSON("dictionary");
-  dictionaryMeta.value = await loadJSON("dictionary");
+  dictionaryMeta.value = await loadJSON("dictionaryMeta");
 });
