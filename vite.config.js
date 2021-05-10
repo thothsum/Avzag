578da180c3817c2780c41c3e791ee2c8323e8e6e
alkaitagi <alkaitagi@outlook.com>
2021-05-10 10:40:24 +0300

    Config update

--- vite.config.js
+++ vite.config.js
@@ -2,26 +2,12 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: "/src/",
-      },
-    ],
-  },
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: '@import "./src/scss/_variables";',
       },
     },
-  },
-  build: {
-    cssCodeSplit: false,
-  },
-  optimizeDeps: {
-    include: ["vue", "vue-router"],
   },
   plugins: [vue()],
 });
