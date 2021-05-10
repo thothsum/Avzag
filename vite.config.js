f3249145ddabf5e7638365bcf89777aa7b1bf349
alkaitagi <alkaitagi@outlook.com>
2021-05-10 10:49:56 +0300

    Install plugin

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import Vue from "@vitejs/plugin-vue";
+import ViteComponents from "vite-plugin-components";
 
 export default defineConfig({
   css: {
@@ -9,5 +10,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue()],
+  plugins: [Vue(), ViteComponents()],
 });
