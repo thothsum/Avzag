c70fe312e4630f4e1cbaa6b3e7ed75afb757a6a5
alkaitagi <alkaitagi@outlook.com>
2021-05-10 09:34:43 +0300

    Fixes

---
+++ src/env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMeta {
+  env: {
+    VITE_STORE?: string;
+    VITE_GH_TOKEN?: string;
+    DEV: boolean;
+  };
+}
--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,11 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  server: {
-    port: 8080,
-  },
   resolve: {
     alias: [
       {
         find: "@",
-        replacement: path.resolve(__dirname, "src"),
+        replacement: "/src/",
       },
     ],
   },
