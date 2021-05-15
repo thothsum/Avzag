2a52cdbdce58b734d8d781cf6187afd35522f683
alkaitagi <alkaitagi@outlook.com>
2021-05-15 13:37:53 +0000

    Deploying to dist from  @ 3049c82e5142198da1e5c4a78af6f691eb6422f2 🚀

--- assets/Flag.f8e3f9e0.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./index.327f15ff.js";import{e,d as r,o as a,h as c,k as t,A as o}from"./vendor.c8bf7c27.js";var f=e({props:{lect:{type:String,default:""}},setup:e=>({src:r((()=>s+e.lect+"/flag.png"))})});const l=o()(((s,e,r,o,f,l)=>s.lect?(a(),c("img",{key:0,src:s.src,class:"no-select"},null,8,["src"])):t("",!0)));f.render=l,f.__scopeId="data-v-078554fb";export{f as _};
